34f10e859883b10cc3103c2ea14895bf5d816503
Nathan Brauner <20107129@mail.wit.ie>
2024-11-21 12:04:20 +0000

    Writing Skeleton Code for Menu Option in Car App

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,59 @@
 package ie.setu
 
+import java.lang.System.exit
+
+fun mainMenu(): Int {
+    print(""" 
+         > ----------------------------------
+         > |        CAR SERVICE APP         |
+         > ----------------------------------
+         > | CAR MENU                       |
+         > |   1) Add a Car                 |
+         > |   2) List all Cars             |
+         > |   3) Update a Car              |
+         > |   4) Delete a Car              |
+         > ----------------------------------
+         > |   0) Exit                      |
+         > ----------------------------------
+         > ==>> """.trimMargin(">"))
+    return readlnOrNull()?.toIntOrNull() ?: -1
+}
+
+fun runMenu() {
+    do {
+        val option = mainMenu()
+        when (option) {
+            1  -> addCar()
+            2  -> listCars()
+            3  -> updateCar()
+            4  -> deleteCar()
+            0  -> exitApp()
+            else -> println("Invalid option entered: $option")
+        }
+    } while (true)
+}
+
+fun addCar(){
+    println("You chose Add Car")
+}
+
+fun listCars(){
+    println("You chose List Cars")
+}
+
+fun updateCar(){
+    println("You chose Update Car")
+}
+
+fun deleteCar(){
+    println("You chose Delete Car")
+}
+
+fun exitApp(){
+    println("Exiting...bye")
+    exit(0)
+}
+
 fun main() {
-    println("Hello World!")
+    runMenu()
 }
