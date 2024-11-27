d8721a2f412223fc7197aaccea8999cf42ca9ce0
Nathan Brauner <20107129@mail.wit.ie>
2024-11-27 13:42:56 +0000

    JUnit Tests for adding to the collection.

--- src/test/kotlin/controllers/CarAPITest.kt
+++ src/test/kotlin/controllers/CarAPITest.kt
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 class CarAPITest {
@@ -23,7 +24,7 @@ class CarAPITest {
         serviceCar = Car("Servicing Car", 1, "125.00", false)
         testDrive = Car("Test Driving Car Before Purchase", 2, "10.00", true)
         repairCar = Car("Repair Car", 3, "50.00", true)
-        testCar =  Car("Testing Car", 6, "80.00", true)
+        testCar = Car("Testing Car", 6, "80.00", true)
         cleanCar = Car("Cleaning Car", 4, "20.00", true)
 
         // adding 5 different notes to the car API
@@ -36,7 +37,7 @@ class CarAPITest {
 
     //The method ran after each test
     @AfterEach
-    fun tearDown(){
+    fun tearDown() {
         serviceCar = null
         testDrive = null
         repairCar = null
@@ -46,23 +47,50 @@ class CarAPITest {
         soldCars = null
     }
 
-    @Test
-    fun `adding a Car to an available list adds to ArrayList`(){
-        val newCar = Car("Checking Tyre Pressure", 2, "25.00", true)
-        assertEquals(5, availableCars!!.numberOfCars())
-        assertTrue(availableCars!!.add(newCar))
-        assertEquals(6, availableCars!!.numberOfCars())
-        assertEquals(newCar, availableCars!!.findCar(availableCars!!.numberOfCars() - 1))
+    @Nested
+    inner class AddCars {
+
+        @Test
+        fun `adding a Car to an available list adds to ArrayList`() {
+            val newCar = Car("Checking Tyre Pressure", 2, "25.00", true)
+            assertEquals(5, availableCars!!.numberOfCars())
+            assertTrue(availableCars!!.add(newCar))
+            assertEquals(6, availableCars!!.numberOfCars())
+            assertEquals(newCar, availableCars!!.findCar(availableCars!!.numberOfCars() - 1))
+        }
+
+        @Test
+        fun `adding a Car to an sold list adds to ArrayList`() {
+            val newCar = Car("Checking Tyre Pressure", 2, "25.00", true)
+            assertEquals(0, soldCars!!.numberOfCars())
+            assertTrue(soldCars!!.add(newCar))
+            assertEquals(1, soldCars!!.numberOfCars())
+            assertEquals(newCar, soldCars!!.findCar(soldCars!!.numberOfCars() - 1))
+        }
     }
 
-    @Test
-    fun `adding a Car to an sold list adds to ArrayList`(){
-        val newCar = Car("Checking Tyre Pressure", 2, "25.00", true)
-        assertEquals(0, soldCars!!.numberOfCars())
-        assertTrue(soldCars!!.add(newCar))
-        assertEquals(1, soldCars!!.numberOfCars())
-        assertEquals(newCar, soldCars!!.findCar(soldCars!!.numberOfCars() - 1))
+    @Nested
+    inner class ListCars {
+
+        @Test
+        fun `listAllCars returns No Cars Stored message when ArrayList is empty`() {
+            assertEquals(0, soldCars!!.numberOfCars())
+            assertTrue(soldCars!!.listAllCars().lowercase().contains("no cars"))
+        }
+
+        @Test
+        fun `listAllCars returns Cars when ArrayList has cars stored`() {
+            assertEquals(5, availableCars!!.numberOfCars())
+            val carsString = availableCars!!.listAllCars().lowercase
+            assertTrue(carsString.contains("Service Car"))
+            assertTrue(carsString.contains("Test Drive"))
+            assertTrue(carsString.contains("Repair Car"))
+            assertTrue(carsString.contains("Test Car"))
+            assertTrue(carsString.contains("Clean Car"))
+        }
     }
 }
+
+
 
 
